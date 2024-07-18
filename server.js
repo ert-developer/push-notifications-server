018ba8c2ca15575d0a15a454556249966187d822
sankeerth-g7 <sankeertherra01@gmail.com>
2024-07-18 14:06:13 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+
+// Parse service account key from environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+app.post('/sendNotification', async (req, res) => {
+  const { token, message } = req.body;
+
+  const payload = {
+    notification: {
+      title: 'New Job Application',
+      body: message,
+    },
+  };
+
+  try {
+    await admin.messaging().sendToDevice(token, payload);
+    res.status(200).send('Notification sent successfully');
+  } catch (error) {
+    console.error('Error sending notification:', error);
+    res.status(500).send('Error sending notification');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
